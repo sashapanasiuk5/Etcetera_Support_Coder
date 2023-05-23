dfb5c8eb33d0f8f02947352a8fcec48e1a8eb959
sashapanasiuk5 <51517423+sashapanasiuk5@users.noreply.github.com>
2023-05-23 19:41:21 +0300

    Update api.py

--- api.py
+++ api.py
@@ -1,12 +1,12 @@
 from flask import Flask
-import subprocess
+from script import Task
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-	return "<h1>Hello World</h1>"
-    subprocess.call("test.py", shell=True)
+    Task()
+    return 'Task Done!'
 
 if __name__ == '__main__':
 	app.run(port=8000,debug=True)
